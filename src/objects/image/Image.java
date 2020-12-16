8e8b279a8e64c26d776ea2a67d60987385991231
Mircea Anton <mircea.anton99@yahoo.com>
2020-12-16 22:01:08 +0000

    Implement private fields with accessors

--- src/objects/image/Image.java
+++ src/objects/image/Image.java
@@ -1,7 +1,22 @@
 package objects.image;
 
 public class Image {
+	// Fields
+	private int width; 			// image width in no. of pixels
+	private int height;			// image height in no. of pixels
+	private Pixel[][] pixels;	// image itself, as pixel matrixx
+
+	// Getters
+	public int getWidth() { return width; }
+	public int getHeight() { return height; }
+	public Pixel[][] getPixels() { return pixels; }
+	public Pixel getPixelAt(int lin, int col) { return pixels[lin][col]; }
+
+	// Setters
+	public void setPixelAt(int lin, int col, Pixel pixel) { pixels[lin][col] = pixel; }
+
+	// Constructors
 	public Image() {
-		
+
 	}
 }
