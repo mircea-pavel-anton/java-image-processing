3dbf327ec4e388d1ac2c0d78e582e28f041fd4c0
Mircea Anton <mircea.anton99@yahoo.com>
2020-12-16 21:03:31 +0000

    Create rough sketch of arguments parsing

--- src/App.java
+++ src/App.java
@@ -1,6 +1,15 @@
 public class App {
 	
 	public static void main(String [] args) {
-		System.out.println("Hello world");
+		String inputFilePath = null;
+		String outputFilePath = null;
+		String filterName = null;
+
+		// TODO parse args
+
+		System.out.println("Args:");
+		System.out.println(inputFilePath);
+		System.out.println(outputFilePath);
+		System.out.println(filterName);
 	}
 }
