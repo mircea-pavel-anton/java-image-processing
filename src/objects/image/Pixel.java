a24dcd0c8a89c389697159607c54620b508738a2
Mircea Anton <mircea.anton99@yahoo.com>
2020-12-16 21:53:59 +0000

    Implement color fields and accessors

--- src/objects/image/Pixel.java
+++ src/objects/image/Pixel.java
@@ -1,5 +1,19 @@
 package objects.image;
 
 public class Pixel {
+	// Color values for each channel
+	private int red;
+	private int green;
+	private int blue;
+
+	// Getters
+	public int getRedChannel() { return red; }
+	public int getGreenChannel() { return green; }
+	public int getBlueChannel()	{ return blue; }
+
+	// Setters
+	public void setRedChannel(int red) { this.red = red; }
+	public void setGreenChannel(int green) { this.green = green; }
+	public void setBlueChannel(int blue) { this.blue = blue; }
 	
 }
