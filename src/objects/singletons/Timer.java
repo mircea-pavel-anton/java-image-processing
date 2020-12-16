8bfd5e910fff5228666a4f69aabf99c7963ced75
Mircea Anton <mircea.anton99@yahoo.com>
2020-12-16 21:23:21 +0000

    Create hashmaps to store time in millis

--- src/objects/singletons/Timer.java
+++ src/objects/singletons/Timer.java
@@ -1,5 +1,7 @@
 package objects.singletons;
 
+import java.util.HashMap;
+
 public class Timer {
 	// Singletion Instance
 	private static Timer instance = null;
@@ -11,5 +13,18 @@ public class Timer {
 	}
 	
 	// Constructor
-	private Timer() {}
+	private Timer() {
+		jobStart = new HashMap<>();
+		jobEnd = new HashMap<>();
+	}
+
+	// Fields
+	// This hashmap will store a key-value pair, the key being the name of the job and the
+	// value being the time in millis at which it was started
+	private HashMap<String, Long> jobStart;
+	// This hashmap will store a key-value pair, the key being the name of the job and the
+	// value being the time in millis at which it was ended
+	private HashMap<String, Long> jobEnd;
+	// In order to get the job duration, we can subtract jobEnd[jobName] - jobStart[jobName]
+	// To get the status of a job, we can see if it exists in either of the hashmaps
 }
