20f5499135029d117177b9fc0a70f6faa0486468
Mircea Anton <mircea.anton99@yahoo.com>
2020-12-18 20:00:36 +0000

    Implement filter method

--- src/objects/filters/ContrastFilter.java
+++ src/objects/filters/ContrastFilter.java
@@ -1,13 +1,29 @@
 package objects.filters;
 
 import objects.image.Image;
+import objects.image.Pixel;
 
 public class ContrastFilter extends GenericFilter {
+	private double factor;
+
+	public ContrastFilter(double contrastLevel) {
+		this.factor = 259 * (contrastLevel + 255) / ( 255 * (259 - contrastLevel) );
+	}
 
 	@Override
 	public Image filter(Image image) {
-		// TODO Auto-generated method stub
-		return null;
+		for (int x = 0; x < image.getWidth(); x++) {
+			for (int y = 0; y < image.getHeight(); y++) {
+				Pixel pixel = image.getPixelAt(x, y);
+				pixel = new Pixel(
+					limit( factor * (pixel.getRedChannel() - 128) + 128 ),
+					limit( factor * (pixel.getGreenChannel() - 128) + 128 ),
+					limit( factor * (pixel.getBlueChannel() - 128) + 128 )
+				);
+				image.setPixelAt( x, y, pixel);
+			}
+		}
+		return image;
 	}
 
 	@Override
