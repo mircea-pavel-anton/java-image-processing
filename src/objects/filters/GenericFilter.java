cd040aebf9fa9fdd0afc27a09b34cd94bb9d462e
Mircea Anton <mircea.anton99@yahoo.com>
2020-12-18 13:45:24 +0000

    Create an Abstract class for filters to implement the apply method

---
+++ src/objects/filters/GenericFilter.java
@@ -0,0 +1,25 @@
+package objects.filters;
+
+import objects.GenericJob;
+import objects.image.Image;
+import objects.singletons.Timer;
+
+public abstract class GenericFilter extends GenericJob implements IFilter {
+	
+	/** 
+	 * Runs the `filter` method from the interface, and stores the execution time into the
+	 * duration field, provided by GenericJob.
+	 * 
+	 * @return -> the filtered image, as returned by the `filter` method from IFilter
+	 */
+	@Override
+	public Image apply(Image image) throws Exception{
+		String identifier = getType() + getUID();
+		Timer timer = Timer.getInstance();
+		timer.startJob( identifier );
+		Image filteredImage = filter(image);
+		duration = timer.stopJob( identifier );
+		return filteredImage;
+	}
+
+}
