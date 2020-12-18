5165c3d1c2e7deb0d53ff56b62d3dd3ff7fc4863
Mircea Anton <mircea.anton99@yahoo.com>
2020-12-18 13:57:23 +0000

    Implement a quick way to test filters

--- src/App.java
+++ src/App.java
@@ -1,3 +1,11 @@
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import objects.filters.GenericFilter;
+import objects.filters.NegativeFilter;
+import objects.image.Image;
 import objects.singletons.ArgParser;
 
 public class App {
@@ -15,5 +23,15 @@ public class App {
 		System.out.println(argParser.getInputFilePath());
 		System.out.println(argParser.getOutputFilePath());
 		System.out.println(argParser.getFilterType());
+
+		try {
+			Image image = new Image(argParser.getInputFilePath());
+			GenericFilter filter = new NegativeFilter();
+			Image filteredImage = filter.filter(image);
+			ImageIO.write(filteredImage.toBufferedImage(), "bmp", new File(argParser.getOutputFilePath()));
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
 	}
 }
