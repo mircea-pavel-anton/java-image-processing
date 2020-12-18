2a0b64ca589fad9271295a1da4ac47ac14a48fbb
Mircea Anton <mircea.anton99@yahoo.com>
2020-12-18 17:29:50 +0000

    Implement filter method

--- src/objects/filters/BrightnessFilter.java
+++ src/objects/filters/BrightnessFilter.java
@@ -1,6 +1,7 @@
 package objects.filters;
 
 import objects.image.Image;
+import objects.image.Pixel;
 
 public class BrightnessFilter extends GenericFilter {
 	private int brightness_delta = 0;
@@ -13,10 +14,26 @@ public class BrightnessFilter extends GenericFilter {
 		}
 	}
 
+	private int limit(int value) {
+		if (value > 255) return 255;
+		if (value < 0) return 0;
+		return value;
+	}
+
 	@Override
 	public Image filter(Image image) {
-		// TODO Auto-generated method stub
-		return null;
+		for (int x = 0; x < image.getWidth(); x++) {
+			for (int y = 0; y < image.getHeight(); y++) {
+				Pixel pixel = image.getPixelAt(x, y);
+				pixel = new Pixel(
+					limit(pixel.getRedChannel() + brightness_delta),
+					limit(pixel.getGreenChannel() + brightness_delta),
+					limit(pixel.getBlueChannel() + brightness_delta)
+				);
+				image.setPixelAt( x, y, pixel);
+			}
+		}
+		return image;
 	}
 
 	@Override
