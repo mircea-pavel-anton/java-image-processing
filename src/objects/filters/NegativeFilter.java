9202928198111032f740eabd43d89a976b0869a9
Mircea Anton <mircea.anton99@yahoo.com>
2020-12-18 13:52:33 +0000

    Impleement filter method

--- src/objects/filters/NegativeFilter.java
+++ src/objects/filters/NegativeFilter.java
@@ -1,13 +1,24 @@
 package objects.filters;
 
 import objects.image.Image;
+import objects.image.Pixel;
 
 public class NegativeFilter extends GenericFilter {
 
 	@Override
 	public Image filter(Image image) {
-		// TODO Auto-generated method stub
-		return null;
+		for (int x = 0; x < image.getWidth(); x++) {
+			for (int y = 0; y < image.getHeight(); y++) {
+				Pixel pixel = image.getPixelAt(x, y);
+				pixel = new Pixel(
+					255 - pixel.getRedChannel(),
+					255 - pixel.getGreenChannel(),
+					255 - pixel.getBlueChannel()
+				);
+				image.setPixelAt( x, y, pixel);
+			}
+		}
+		return image;
 	}
 
 	@Override
