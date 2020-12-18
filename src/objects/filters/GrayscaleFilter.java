b66dd43fc9b5b8ecd47044aca7cc5a721c6b0d36
Mircea Anton <mircea.anton99@yahoo.com>
2020-12-18 13:36:03 +0000

    Implement the 'apply' method

--- src/objects/filters/GrayscaleFilter.java
+++ src/objects/filters/GrayscaleFilter.java
@@ -2,13 +2,18 @@ package objects.filters;
 
 import objects.GenericJob;
 import objects.image.Image;
+import objects.singletons.Timer;
 
 public class GrayscaleFilter extends GenericJob implements IFilter {
 
 	@Override
-	public Image apply(Image image) {
-		// TODO Auto-generated method stub
-		return null;
+	public Image apply(Image image) throws Exception{
+		String identifier = getType() + getUID();
+		Timer timer = Timer.getInstance();
+		timer.startJob( identifier );
+		Image filteredImage = filter(image);
+		duration = timer.stopJob( identifier );
+		return filteredImage;
 	}
 
 	@Override
@@ -20,7 +25,5 @@ public class GrayscaleFilter extends GenericJob implements IFilter {
 	@Override
 	public String getType() { return GRAYSCALE_FILTER; }
 
-	public GrayscaleFilter() {
-		
-	}
+	public GrayscaleFilter() {}
 }
