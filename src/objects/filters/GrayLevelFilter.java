2a6a734b6a3b25d288296d16a6f39d5c0f9fb91a
Mircea Anton <mircea.anton99@yahoo.com>
2020-12-18 22:13:13 +0000

    Implement logarithmic gray level transform

--- src/objects/filters/GrayLevelFilter.java
+++ src/objects/filters/GrayLevelFilter.java
@@ -13,12 +13,21 @@ public class GrayLevelFilter extends GenericFilter {
 		);
 	}
 
+	private Pixel logarithmicTransform(Pixel input) {
+		double c = 50;
+		return new Pixel(
+			limit( c * Math.log( (double)input.getRedChannel() + 1 ) ),
+			limit( c * Math.log( (double)input.getGreenChannel() + 1 ) ),
+			limit( c * Math.log( (double)input.getBlueChannel() + 1 ) )
+		);
+	}
+
 	@Override
 	public Image filter(Image image) {
 		Pixel[][] pixels = image.getPixels();
 		for (int x = 0; x < image.getWidth(); x++) {
 			for (int y = 0; y < image.getHeight(); y++) {
-				pixels[x][y] = linearTransform(pixels[x][y]);
+				pixels[x][y] = logarithmicTransform(pixels[x][y]);
 			}
 		}
 		return new Image(pixels);
