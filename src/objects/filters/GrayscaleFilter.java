ab173689fd454e206fd8a996d98f96c71875850f
Mircea Anton <mircea.anton99@yahoo.com>
2020-12-18 13:38:49 +0000

    Implement 'filter' method

--- src/objects/filters/GrayscaleFilter.java
+++ src/objects/filters/GrayscaleFilter.java
@@ -2,6 +2,7 @@ package objects.filters;
 
 import objects.GenericJob;
 import objects.image.Image;
+import objects.image.Pixel;
 import objects.singletons.Timer;
 
 public class GrayscaleFilter extends GenericJob implements IFilter {
@@ -18,8 +19,17 @@ public class GrayscaleFilter extends GenericJob implements IFilter {
 
 	@Override
 	public Image filter(Image image) {
-		// TODO Auto-generated method stub
-		return null;
+		for (int x = 0; x < image.getWidth(); x++) {
+			for (int y = 0; y < image.getHeight(); y++) {
+				Pixel pixel = image.getPixelAt(x, y);
+				int grayscale = 0;
+				grayscale += ( pixel.getRedChannel() / 3 );
+				grayscale += ( pixel.getGreenChannel() / 3 );
+				grayscale += ( pixel.getBlueChannel() / 3 );
+				image.setPixelAt(x, y, new Pixel(grayscale));
+			}
+		}
+		return image;
 	}
 
 	@Override
