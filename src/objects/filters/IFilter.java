473b6bf7b34b823a5ccb654ca314099f29099f3f
Mircea Anton <mircea.anton99@yahoo.com>
2020-12-18 13:28:45 +0000

    Implement const names and filter method

--- src/objects/filters/IFilter.java
+++ src/objects/filters/IFilter.java
@@ -3,5 +3,24 @@ package objects.filters;
 import objects.image.Image;
 
 public interface IFilter {
+	// All filter type names as constants
+	public static final String GRAYSCALE_FILTER = "grayscale";
+
+
+	/** Runs the `filter` method and tracks execution time
+	 * 
+	 * @param image -> the image to be processed
+	 * @return -> the processed image, as returned by `filter`
+	 */
 	public Image apply(Image image);
+
+	/** Applies the operations associated to the filter on the given image, and returns the result
+	 * 
+	 * @param image -> the image to be processed
+	 * @return -> the processed image
+	 */
+	public Image filter(Image image);
+
+	/** Returns a string identifier for each specific kind of filter */
+	public String getType();
 }
