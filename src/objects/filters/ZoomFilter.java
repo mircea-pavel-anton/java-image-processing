ef42ba69ef772944147b8efc318467db71107721
Mircea Anton <mircea.anton99@yahoo.com>
2020-12-18 21:35:23 +0000

    Implement pixel replicaton zooming method

--- src/objects/filters/ZoomFilter.java
+++ src/objects/filters/ZoomFilter.java
@@ -1,13 +1,34 @@
 package objects.filters;
 
 import objects.image.Image;
+import objects.image.Pixel;
 
 public class ZoomFilter extends GenericFilter {
 
+	private int zoom;
+	
+	public ZoomFilter(int zoom) {
+		if (zoom > 0) {
+			this.zoom = zoom;
+		} else {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private Pixel[][] pixelReplication(Pixel[][] original, int width, int height) {
+		Pixel[][] pixels = new Pixel[width * zoom][height * zoom];
+
+		for (int x = 0; x < width * zoom; x++) {
+			for (int  y = 0; y < height * zoom; y++) {
+				pixels[x][y] = original[x / zoom][y / zoom];
+			}
+		}
+		return pixels;
+	}
+
 	@Override
 	public Image filter(Image image) {
-		// TODO Auto-generated method stub
-		return null;
+		return new Image(pixelReplication(image.getPixels(), image.getWidth(), image.getHeight()));
 	}
 
 	@Override
