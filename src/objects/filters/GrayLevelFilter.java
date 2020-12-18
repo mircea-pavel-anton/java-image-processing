a7d265c5b1710339bdf6ea26cef32ff55f76005b
Mircea Anton <mircea.anton99@yahoo.com>
2020-12-18 22:02:47 +0000

    Implement linear gray level transform

--- src/objects/filters/GrayLevelFilter.java
+++ src/objects/filters/GrayLevelFilter.java
@@ -1,13 +1,27 @@
 package objects.filters;
 
 import objects.image.Image;
+import objects.image.Pixel;
 
 public class GrayLevelFilter extends GenericFilter {
 
+	private Pixel linearTransform(Pixel input) {
+		return new Pixel(
+			255 - input.getRedChannel(),
+			255 - input.getGreenChannel(),
+			255 - input.getBlueChannel()
+		);
+	}
+
 	@Override
 	public Image filter(Image image) {
-		// TODO Auto-generated method stub
-		return null;
+		Pixel[][] pixels = image.getPixels();
+		for (int x = 0; x < image.getWidth(); x++) {
+			for (int y = 0; y < image.getHeight(); y++) {
+				pixels[x][y] = linearTransform(pixels[x][y]);
+			}
+		}
+		return new Image(pixels);
 	}
 
 	@Override
