8f6d4b334947dd3249600f81d1ca59d278e626e0
Mircea Anton <mircea.anton99@yahoo.com>
2020-12-18 14:45:13 +0000

    Implement filter method

--- src/objects/filters/GrayscaleToBinaryFilter.java
+++ src/objects/filters/GrayscaleToBinaryFilter.java
@@ -1,8 +1,9 @@
 package objects.filters;
 
 import objects.image.Image;
+import objects.image.Pixel;
 
-public class GrayscaleToBinaryFilter extends GenericFilter {)
+public class GrayscaleToBinaryFilter extends GenericFilter {
 	private int threshold;
 
 	public GrayscaleToBinaryFilter(int threshold) {
@@ -15,8 +16,17 @@ public class GrayscaleToBinaryFilter extends GenericFilter {)
 
 	@Override
 	public Image filter(Image image) {
-		// TODO Auto-generated method stub
-		return null;
+		for (int x = 0; x < image.getWidth(); x++) {
+			for (int y = 0; y < image.getHeight(); y++) {
+				Pixel pixel = image.getPixelAt(x, y);
+				if (pixel.getBlueChannel() < threshold) {
+					image.setPixelAt(x, y, new Pixel(0));
+				} else {
+					image.setPixelAt(x, y, new Pixel(255));
+				}
+			}
+		}
+		return image;
 	}
 
 	@Override
