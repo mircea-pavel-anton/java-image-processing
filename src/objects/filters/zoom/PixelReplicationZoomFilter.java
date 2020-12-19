b021d81e1ba7d43c6fc20344d50c817e5ada548a
Mircea Anton <mircea.anton99@yahoo.com>
2020-12-19 01:34:42 +0000

    Implement constructor

--- src/objects/filters/zoom/PixelReplicationZoomFilter.java
+++ src/objects/filters/zoom/PixelReplicationZoomFilter.java
@@ -3,7 +3,13 @@ package objects.filters.zoom;
 import objects.image.Pixel;
 
 public class PixelReplicationZoomFilter extends AbstractZoomFilter {
-
+	public PixelReplicationZoomFilter(int zoomLevel) {
+		if (zoomLevel > 0) {
+			this.zoomLevel = zoomLevel;
+		} else {
+			throw new IllegalArgumentException();
+		}
+	}
 
 	/** Pixel replication zooming algorithm.
 	 * 
@@ -11,18 +17,18 @@ public class PixelReplicationZoomFilter extends AbstractZoomFilter {
 	 * No fancy logic is needed, as x/n and y/n only actually increment when x and y are
 	 * have already traversed the n x n matrix.
 	 * 
-	 * @param original -> the original matrix
+	 * @param input -> the original matrix
 	 * @return -> the zoomed matrix
 	 */
 	@Override
-	protected Pixel[][] zoom(Pixel[][] original) {
-		int width  = original.length;
-		int height = original[0].length;
+	protected Pixel[][] zoom(Pixel[][] input) {
+		int width  = input.length;
+		int height = input[0].length;
 		Pixel[][] pixels = new Pixel[width * zoomLevel][height * zoomLevel];
 
 		for (int x = 0; x < width * zoomLevel; x++) {
 			for (int  y = 0; y < height * zoomLevel; y++) {
-				pixels[x][y] = original[x / zoomLevel][y / zoomLevel];
+				pixels[x][y] = input[x / zoomLevel][y / zoomLevel];
 			}
 		}
 
