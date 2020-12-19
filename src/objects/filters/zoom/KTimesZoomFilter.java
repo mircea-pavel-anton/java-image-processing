4d1fa59f037095dececc4e607de4a5c2bb4e4e0e
Mircea Anton <mircea.anton99@yahoo.com>
2020-12-19 01:37:09 +0000

    Create KTimes zoom filter

---
+++ src/objects/filters/zoom/KTimesZoomFilter.java
@@ -0,0 +1,13 @@
+package objects.filters.zoom;
+
+import objects.image.Pixel;
+
+public class KTimesZoomFilter extends AbstractZoomFilter {
+
+	@Override
+	protected Pixel[][] zoom(Pixel[][] input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
