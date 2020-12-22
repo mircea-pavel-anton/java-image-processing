4a20b706e8b833aa1f6e4dc2b0bc76f5105cae71
Mircea Anton <mircea.anton99@yahoo.com>
2020-12-22 12:10:59 +0000

    Implement the filter list and the run method

--- src/objects/singletons/FilterBuilder.java
+++ src/objects/singletons/FilterBuilder.java
@@ -1,6 +1,11 @@
 package objects.singletons;
 
-public class FilterBuilder {
+import java.util.ArrayList;
+
+import objects.GenericJob;
+import objects.filters.GenericFilter;
+
+public class FilterBuilder extends GenericJob {
 	// Singleton Instance
 	private static FilterBuilder instance = null;
 
@@ -18,4 +23,27 @@ public class FilterBuilder {
 
 	// Constructor
 	private FilterBuilder() {} // private constructor, meant to hide the default, public one
+
+	// Fields
+	private ArrayList<GenericFilter> filters = new ArrayList<GenericFilter>();
+
+	/** Getter for the list of filters */
+	public ArrayList<GenericFilter> getFilters() { return this.filters; }
+
+	/** Interactive shell sequence that prompts the user to build up filters */
+	private void build() {
+
+	}
+
+	/** A wrapper for the @build function that implements the Timer singleton to track execution
+	 * duration
+	 * 
+	 * @throws Exception -> rethrows all exceptions
+	 */
+	public void run() throws Exception {
+		Timer timer = Timer.getInstance();
+		timer.startJob(String.valueOf(uID));
+		build();
+		duration = timer.stopJob(String.valueOf(uID));
+	}
 }
