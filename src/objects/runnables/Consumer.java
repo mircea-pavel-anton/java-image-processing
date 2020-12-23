e3b96ded7afa422c387b31aae4c42f5667e0a0c8
Mircea Anton <mircea.anton99@yahoo.com>
2020-12-23 17:37:48 +0000

    MAJOR code cleanup

--- src/objects/runnables/Consumer.java
+++ src/objects/runnables/Consumer.java
@@ -13,12 +13,264 @@ public class Consumer implements Runnable {
 	private int x;
 	private int y;
 	private int character;
+	private boolean isDone;
 
 	public Consumer(BlockingQueue<Integer> queue, BitmapImage image) {
 		this.bQueue = queue;
 		this.bitmapImage = image;
+
+		// initialize values
 		charCount = 0;
 		temp = "";
+		isDone = false;
+	}
+
+
+	/** The first state of the modelled FSM that handles parsing the Raw Pixel Data
+	 * The FSM models a nested for loop, that executes one iteration per
+	 * extraction from the BlockingQueue
+	 * 
+	 * Since this is the first state, we also need to initailize the pixel in our
+	 * matrix.
+	 * In this step, we read the first of 3 (or 4) bytes that make up our pixel data
+	 * This first byte represents the inensity of the blue color
+	 * As such, we create a new pixel, and set the blue channel = character
+	 */
+	private void FSM_State1() { bitmapImage.setPixelAt(x, y, new Pixel(character)); }
+
+	/** The second state of the modelled FSM that handles parsing the Raw Pixel Data
+	 * The FSM models a nested for loop, that executes one iteration per
+	 * extraction from the BlockingQueue
+	 * 
+	 * The second state handles the second byte, which represents the intensity
+	 * value for the green color
+	 * No further actions are required, other than setting the green color channel
+	 * of our pixel = character
+	 */
+	private void FSM_State2() { bitmapImage.getPixelAt(x, y).setGreen(character); }
+
+	/** The third state of the modelled FSM that handles parsing the Raw Pixel Data
+	 * The FSM models a nested for loop, that executes one iteration per
+	 * extraction from the BlockingQueue
+	 * 
+	 * The third state handles the third byte, which represents the intensity
+	 * value for the red color, so we set the pixel's red channel value to be
+	 * equal to character
+	 * 
+	 * Other than that, if the image we are reading has 24 bits-per-pixel
+	 * (3 bytes per pixel), we need to increment our matrix coordinates
+	 * in order to move to the next pixel
+	 */
+	private void FSM_State3() {
+		bitmapImage.getPixelAt(x, y).setRed(character);
+		if (bitmapImage.getBytesPerPixel() == 3) increment();
+	}
+
+	/** The fourth state of the modelled FSM that handles parsing the Raw Pixel Data
+	 * The FSM models a nested for loop, that executes one iteration per
+	 * extraction from the BlockingQueue
+	 * 
+	 * The fourth state handles the fourth byte, which represents the intensity
+	 * value for the alpha channel, so we set the pixel's alpha value to be
+	 * equal to character
+	 * 
+	 * This state is only used in the case of a 32-bit BMP file
+	 * If we reach this state, we need to increment our matrix coordinates
+	 * to move to the next pixel, since state 3 did not pass the condition
+	 * for incrementing
+	 */
+	private void FSM_State4() {
+		bitmapImage.getPixelAt(x, y).setAlpha(character);
+		increment();
+	}
+
+
+	/** Handles matrix coordinate incrementation, based on the modelled
+	 * nested for loop:
+	 * for (int x = 0; x < width -1; x++) {
+	 *     for (int y = height-1; y > -1; y--){
+	 *         // read pixel data
+	 *     }
+	 * }
+	 * isDone keeps track wheter or not the loop has finished all its
+	 * iterations
+	 */
+	private void increment() {
+		if ( x == bitmapImage.getImageWidth() - 1 ) {
+			y--;
+			x = 0;
+		} else {
+			x++;
+		}
+		if ( y == -1 ) { isDone = true; }
+	}
+
+	/** Handles parsing the bits from the BMP header, based on their index 
+	 * See this medium.com article for more details on header format:
+	 * https://medium.com/sysf/bits-to-bitmaps-a-simple-walkthrough-of-bmp-image-format-765dc6857393
+	*/
+	private void readBmpHeader() {
+		switch (charCount) {
+			case 1: temp = String.format("%c", character); break;
+			case 2: 
+				temp += String.format("%c", character);
+				bitmapImage.setFileType(temp);
+				break;
+
+			case 3: temp = String.format("%02X", character); break;
+			case 4: temp = String.format("%02X", character) + temp; break;
+			case 5: temp = String.format("%02X", character) + temp; break;
+			case 6:
+				temp = String.format("%02X", character) + temp;
+				bitmapImage.setFileSize( Integer.parseInt(temp, 16) );
+				break;
+
+			case 7: temp = String.format("%c", character); break;
+			case 8: 
+				temp += String.format("%c", character);
+				bitmapImage.setReserved1(temp);
+				break;
+			case 9: temp = String.format("%c", character); break;
+			case 10: 
+				temp += String.format("%c", character);
+				bitmapImage.setReserved2(temp);
+				break;
+
+			case 11: temp = String.format("%02X", character); break;
+			case 12: temp = String.format("%02X", character) + temp; break;
+			case 13: temp = String.format("%02X", character) + temp; break;
+			case 14:
+				temp = String.format("%02X", character) + temp;
+				bitmapImage.setPixelDataOffset( Integer.parseInt(temp, 16) );
+				break;
+		}
+	}
+
+	/** Handles parsing the bits from the DIB header, based on their index 
+	 * See this medium.com article for more details on header format:
+	 * https://medium.com/sysf/bits-to-bitmaps-a-simple-walkthrough-of-bmp-image-format-765dc6857393
+	*/
+	private void readDibHeader() {
+		switch (charCount) {
+			case 15: temp = String.format("%02X", character); break;
+			case 16: temp = String.format("%02X", character) + temp; break;
+			case 17: temp = String.format("%02X", character) + temp; break;
+			case 18:
+				temp = String.format("%02X", character) + temp;
+				bitmapImage.setHeaderSize( Integer.parseInt(temp, 16) );
+				break;
+
+			case 19: temp = String.format("%02X", character); break;
+			case 20: temp = String.format("%02X", character) + temp; break;
+			case 21: temp = String.format("%02X", character) + temp; break;
+			case 22:
+				temp = String.format("%02X", character) + temp;
+				bitmapImage.setImageWidth( Integer.parseInt(temp, 16) );
+				break;
+
+			case 23: temp = String.format("%02X", character); break;
+			case 24: temp = String.format("%02X", character) + temp; break;
+			case 25: temp = String.format("%02X", character) + temp; break;
+			case 26:
+				temp = String.format("%02X", character) + temp;
+				y = Integer.parseInt(temp, 16) - 1;
+				bitmapImage.setImageHeight( y+1 );
+				break;
+
+			case 27: temp = String.format("%02X", character); break;
+			case 28:
+				temp = String.format("%02X", character) + temp;
+				bitmapImage.setPlanes( Integer.parseInt(temp, 16) );
+				break;
+
+			case 29: temp = String.format("%02X", character); break;
+			case 30:
+				temp = String.format("%02X", character) + temp;
+				bitmapImage.setBitsPerPixel( Integer.parseInt(temp, 16) );
+				break;
+
+			case 31: temp = String.format("%02X", character); break;
+			case 32: temp = String.format("%02X", character) + temp; break;
+			case 33: temp = String.format("%02X", character) + temp; break;
+			case 34:
+				temp = String.format("%02X", character) + temp;
+				bitmapImage.setCompression( Integer.parseInt(temp, 16) );
+				break;
+
+			case 35: temp = String.format("%02X", character); break;
+			case 36: temp = String.format("%02X", character) + temp; break;
+			case 37: temp = String.format("%02X", character) + temp; break;
+			case 38:
+				temp = String.format("%02X", character) + temp;
+				bitmapImage.setImageSize( Integer.parseInt(temp, 16) );
+				break;
+
+			case 39: temp = String.format("%02X", character); break;
+			case 40: temp = String.format("%02X", character) + temp; break;
+			case 41: temp = String.format("%02X", character) + temp; break;
+			case 42:
+				temp = String.format("%02X", character) + temp;
+				bitmapImage.setXpixelsPerMeter( Integer.parseInt(temp, 16) );
+				break;
+
+			case 43: temp = String.format("%02X", character); break;
+			case 44: temp = String.format("%02X", character) + temp; break;
+			case 45: temp = String.format("%02X", character) + temp; break;
+			case 46:
+				temp = String.format("%02X", character) + temp;
+				bitmapImage.setYpixelsPerMeter( Integer.parseInt(temp, 16) );
+				break;
+
+			case 47: temp = String.format("%02X", character); break;
+			case 48: temp = String.format("%02X", character) + temp; break;
+			case 49: temp = String.format("%02X", character) + temp; break;
+			case 50:
+				temp = String.format("%02X", character) + temp;
+				bitmapImage.setTotalColors( Integer.parseInt(temp, 16) );
+				break;
+
+			case 51: temp = String.format("%02X", character); break;
+			case 52: temp = String.format("%02X", character) + temp; break;
+			case 53: temp = String.format("%02X", character) + temp; break;
+			case 54:
+				temp = String.format("%02X", character) + temp;
+				bitmapImage.setImportantColors( Integer.parseInt(temp, 16) );
+				break;
+		}
+	}
+
+	/** Handles parsing the bits from the RPD, based on their index 
+	 * See this medium.com article for more details on header format:
+	 * https://medium.com/sysf/bits-to-bitmaps-a-simple-walkthrough-of-bmp-image-format-765dc6857393
+	*/
+	private void readRawPixelData() throws Exception {
+		// init pixel matrix if needed
+		if (bitmapImage.getPixels() == null) bitmapImage.initPixels();
+
+		// position = byte index inside the raw pixel data
+		int position = charCount - bitmapImage.getPixelDataOffset();
+
+		switch (bitmapImage.getBytesPerPixel()) {
+			case 3: // 24 bit BMP
+				switch ( position % 3) {
+					case 1: FSM_State1(); break; // read blue
+					case 2: FSM_State2(); break; // read green
+					case 0: FSM_State3(); break; // read red & increment
+				}
+				if (isDone) return; // if we run out of pixels
+				break;
+			case 4:
+				switch ( position % 4) {
+					case 1: FSM_State1(); break; // read blue
+					case 2: FSM_State2(); break; // read green
+					case 3: FSM_State3(); break; // read red
+					case 4: FSM_State4(); break; // read alpha & increment
+				}
+				if (isDone) return; // if we run out of pixels
+				break;
+			default: // if the image is 16bpp or less
+				throw new Exception("BmpIO only supports 24 and 32 bit BMP images");
+		}
 	}
 
 	@Override
@@ -26,194 +278,26 @@ public class Consumer implements Runnable {
 		while (true) {
 			synchronized(this) {
 				try {
-					// while (bQueue.size() == 0) wait();
-
 					charCount++;
-					System.out.println("Char count: " + charCount);
 					character = bQueue.take();
-	
-					switch (charCount) {
-						case 1: temp = String.format("%c", character); break;
-						case 2: 
-							temp += String.format("%c", character);
-							bitmapImage.setFileType(temp);
-							break;
-	
-						case 3: temp = String.format("%02X", character); break;
-						case 4: temp = String.format("%02X", character) + temp; break;
-						case 5: temp = String.format("%02X", character) + temp; break;
-						case 6:
-							temp = String.format("%02X", character) + temp;
-							bitmapImage.setFileSize( Integer.parseInt(temp, 16) );
-							break;
-	
-						case 7: temp = String.format("%c", character); break;
-						case 8: 
-							temp += String.format("%c", character);
-							bitmapImage.setReserved1(temp);
-							break;
-						case 9: temp = String.format("%c", character); break;
-						case 10: 
-							temp += String.format("%c", character);
-							bitmapImage.setReserved2(temp);
-							break;
-	
-						case 11: temp = String.format("%02X", character); break;
-						case 12: temp = String.format("%02X", character) + temp; break;
-						case 13: temp = String.format("%02X", character) + temp; break;
-						case 14:
-							temp = String.format("%02X", character) + temp;
-							bitmapImage.setPixelDataOffset( Integer.parseInt(temp, 16) );
-							break;
-	
-						case 15: temp = String.format("%02X", character); break;
-						case 16: temp = String.format("%02X", character) + temp; break;
-						case 17: temp = String.format("%02X", character) + temp; break;
-						case 18:
-							temp = String.format("%02X", character) + temp;
-							bitmapImage.setHeaderSize( Integer.parseInt(temp, 16) );
-							break;
-	
-						case 19: temp = String.format("%02X", character); break;
-						case 20: temp = String.format("%02X", character) + temp; break;
-						case 21: temp = String.format("%02X", character) + temp; break;
-						case 22:
-							temp = String.format("%02X", character) + temp;
-							bitmapImage.setImageWidth( Integer.parseInt(temp, 16) );
-							break;
-	
-						case 23: temp = String.format("%02X", character); break;
-						case 24: temp = String.format("%02X", character) + temp; break;
-						case 25: temp = String.format("%02X", character) + temp; break;
-						case 26:
-							temp = String.format("%02X", character) + temp;
-							y = Integer.parseInt(temp, 16) - 1;
-							bitmapImage.setImageHeight( y+1 );
-							break;
-	
-						case 27: temp = String.format("%02X", character); break;
-						case 28:
-							temp = String.format("%02X", character) + temp;
-							bitmapImage.setPlanes( Integer.parseInt(temp, 16) );
-							break;
-	
-						case 29: temp = String.format("%02X", character); break;
-						case 30:
-							temp = String.format("%02X", character) + temp;
-							bitmapImage.setBitsPerPixel( Integer.parseInt(temp, 16) );
-							break;
-	
-						case 31: temp = String.format("%02X", character); break;
-						case 32: temp = String.format("%02X", character) + temp; break;
-						case 33: temp = String.format("%02X", character) + temp; break;
-						case 34:
-							temp = String.format("%02X", character) + temp;
-							bitmapImage.setCompression( Integer.parseInt(temp, 16) );
-							break;
-	
-						case 35: temp = String.format("%02X", character); break;
-						case 36: temp = String.format("%02X", character) + temp; break;
-						case 37: temp = String.format("%02X", character) + temp; break;
-						case 38:
-							temp = String.format("%02X", character) + temp;
-							bitmapImage.setImageSize( Integer.parseInt(temp, 16) );
-							break;
-	
-						case 39: temp = String.format("%02X", character); break;
-						case 40: temp = String.format("%02X", character) + temp; break;
-						case 41: temp = String.format("%02X", character) + temp; break;
-						case 42:
-							temp = String.format("%02X", character) + temp;
-							bitmapImage.setXpixelsPerMeter( Integer.parseInt(temp, 16) );
-							break;
-	
-						case 43: temp = String.format("%02X", character); break;
-						case 44: temp = String.format("%02X", character) + temp; break;
-						case 45: temp = String.format("%02X", character) + temp; break;
-						case 46:
-							temp = String.format("%02X", character) + temp;
-							bitmapImage.setYpixelsPerMeter( Integer.parseInt(temp, 16) );
-							break;
-	
-						case 47: temp = String.format("%02X", character); break;
-						case 48: temp = String.format("%02X", character) + temp; break;
-						case 49: temp = String.format("%02X", character) + temp; break;
-						case 50:
-							temp = String.format("%02X", character) + temp;
-							bitmapImage.setTotalColors( Integer.parseInt(temp, 16) );
-							break;
-	
-						case 51: temp = String.format("%02X", character); break;
-						case 52: temp = String.format("%02X", character) + temp; break;
-						case 53: temp = String.format("%02X", character) + temp; break;
-						case 54:
-							temp = String.format("%02X", character) + temp;
-							bitmapImage.setImportantColors( Integer.parseInt(temp, 16) );
-							break;
-	
-						default: // for any charCount over 54, we are in the Raw Pixel Data
-							// if we just entered the raw pixel data, then we surely have
-							// read the width and height of our image, so we can init the
-							// pixel matrix
-							if (charCount == bitmapImage.getPixelDataOffset() + 1) {
-								bitmapImage.initPixels();
-							}
+					System.out.println("Char count: " + charCount);
 
-							// This next bit of code attempts to simulate a finite state machine
-							// The 3 available states are:
-							//   - state 1: (we're doing a %3, so it's represented by case 1)
-							//      - init the pixel at x,y
-							//      - put the value from the producer into the blue channel
-							//   - state 2: (we're doing a %3, so it's represented by case 1)
-							//      - put the value from the producer into the green channel
-							//   - state 3:
-							if (charCount >= bitmapImage.getPixelDataOffset()) {
-								if (bitmapImage.getBitsPerPixel() == 24) {
-									int position = charCount - bitmapImage.getPixelDataOffset();
-									switch ( position % bitmapImage.getBytesPerPixel() ) {
-										case 1: // the first byte represents the blue color
-											// since it's the first time we read, we need
-											// to create the pixel as well we can initialize
-											// the pixel to have this value on all channels,
-											// and go from there, by replacing the green and
-											//red channels in the next 2 iterations
-											bitmapImage.setPixelAt(x, y, new Pixel(character));
-											break;
-
-										case 2:  // the second byte represents the green color
-											bitmapImage.getPixelAt(x, y).setGreen(character);
-											break;
-
-										case 0: // the third and last byte represents the red color
-											bitmapImage.getPixelAt(x, y).setRed(character);
-
-											// Since we finished reading a pixel, we have to move to the next
-											// one in our matrix.
-											// We are simulating a nested for loop, with:
-											// x from 0 to width -1
-											// y from height -1 to 0
-											if ( x == bitmapImage.getImageWidth() - 1 ) {
-												y--;
-												x = 0;
-											} else {
-												x++;
-											}
-
-											// If we passed the y = 0 mark, then we're
-											// basically out of the nested loop
-											// and we can exit this thread
-											if ( y == -1 ) { return; }
-											break;
-									}
-								}
-							}
-							break;
+					if (charCount > 0 && charCount < 15) {
+						readBmpHeader();
+					} else if (charCount < 55) {
+						readDibHeader();
+					} else {
+						readRawPixelData();
 					}
+
+					if (isDone) return;
 
 					notify();
 				} catch (InterruptedException e) {
 					System.out.println("Such is life... All must die eventually...");
-					// e.printStackTrace();
+					e.printStackTrace();
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
 				}
 			}
 		}
